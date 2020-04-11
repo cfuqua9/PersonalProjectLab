4535d6ccb5879f9491f241e159ad5bdb84f89bab
cfuqua9 <59981807+cfuqua9@users.noreply.github.com>
2020-04-11 11:28:21 -0500

    Update Program.cs

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -2,11 +2,44 @@
 
 namespace PersonalProjectLab
 {
+    using System.IO;
+    using System;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            String input;
+            //prompt the user to enter soup or salad
+            Console.Write("Enter your choice (soup or salad):\t");
+            input = Console.ReadLine();
+            //if the input is soup
+            if (input == "soup")
+            {
+                String item;
+                //prompt the user for the type of soup
+                Console.Write("what kind soup do you want?\t");
+                item = Console.ReadLine();
+                //display the ordered items
+                Console.WriteLine("You have ordered : " + item + " " + input);
+                Console.WriteLine("Thank You For Ordering!");
+            }
+            //if the user choses salad
+            else if (input == "salad")
+            {
+                int num;
+                String toppings;
+                //prompt the user for number of toppings
+                Console.Write("Enter Number of topping :\t");
+                num = Convert.ToInt32(Console.ReadLine());
+                //prompt the user to choose from a list of topppings
+                Console.Write("croutons\ncarrots\neggs\ntomatoes\ntomatoes\nEnter youe choice:\t");
+                toppings = Console.ReadLine();
+                //display the ordered items
+                Console.WriteLine("You have ordered : " + num + " " + toppings + " " + input);
+                Console.WriteLine("Thank You For Ordering!");
+            }
         }
     }
+
 }
