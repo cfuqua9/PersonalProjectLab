75529e5eef23fad29429052b506cf5da2530e576
cfuqua9 <59981807+cfuqua9@users.noreply.github.com>
2020-04-20 18:40:00 -0500

    Update EmptyClass.cs

--- PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
+++ PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 namespace PersonalProjectLab
 {
-    public class EmptyClass
+    public class Order
     {
-        public EmptyClass()
+        string _ordertype = "";
+        string _orderdetail = "";
+        int _numtoppings = 0;
+
+        public Order()
         {
+
+        }
+
+
+        public void addordertype(string ordertype)
+        {
+            _ordertype = ordertype;
+
+        }
+        public void addorderdetail(string orderdetail, int numtoppings)
+        {
+            _orderdetail = orderdetail;
+            _numtoppings = numtoppings;
+        }
+
+        public string getorderdetails()
+        {
+            if (_ordertype=="soup")
+            {
+                return _orderdetail + " " + _ordertype;
+            }
+            else if (_ordertype == "salad")
+            {
+                return _numtoppings + " " + _orderdetail + " " + _ordertype;
+            }
+            return "UnKnown Order";
         }
     }
 }
