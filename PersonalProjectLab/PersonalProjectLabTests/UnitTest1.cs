f5031347c36030d82810adaad36b89735456afdd
cfuqua9 <59981807+cfuqua9@users.noreply.github.com>
2020-04-20 18:40:07 -0500

    Update UnitTest1.cs

--- PersonalProjectLab/PersonalProjectLabTests/UnitTest1.cs
+++ PersonalProjectLab/PersonalProjectLabTests/UnitTest1.cs
@@ -1,13 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using PersonalProjectLab;
 namespace PersonalProjectLabTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SoupMethod()
         {
+            //arrange
+            Order order = new Order();
+
+            //act
+            order.addordertype("soup");
+            order.addorderdetail("tomatoe",0);
+            string details = order.getorderdetails();
+
+            //assert
+            Assert.AreEqual("tomatoe soup", details);
+
+        }
+        [TestMethod]
+        public void SaladMethod()
+        {
+            //arrange
+            Order order = new Order();
+
+            //act
+            order.addordertype("salad");
+            order.addorderdetail("croutons", 0);
+            string toppings = order.getorderdetails();
+
+            //assert
+            Assert.AreEqual("salad with croutons", toppings);
+
         }
     }
 }
